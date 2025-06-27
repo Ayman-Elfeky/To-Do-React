c4be1b909f5b6f52278ce100363e6cc40cd4f471
Ayman Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-06-27 23:06:01 +0300

    Update App.js add Navbar and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import TodoList from './components/TodoList';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer' 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <TodoList />
+      <Footer />
     </div>
   );
 }
